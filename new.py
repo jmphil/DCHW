6dcf6a4435cd5c39e811b19112957537553d19cf
Matt Phillips <spectremp24@gmail.com>
2020-11-12 15:55:56 -0500

    11/12 stuff

---
+++ new.py
@@ -0,0 +1,16 @@
+# new txt file
+# 2nd push text
+
+# danger = range(10)
+
+# range(10,20)
+
+# print(danger)
+
+names = ['George', 'W', 'Bush']
+# for nameElement in names:
+#     print(nameElement)
+
+# for number in range(0, 10, 2):
+#     print(number)
+
